63ab3383c0d11df2a9ecc2dc8bf7a19ab560b438
sebteouii <sebteouiikhoubayb@gmail.com>
2024-03-10 15:33:31 +0100

    a class BaseModel that defines all common attributes/methods for other classes

---
+++ models/base_model.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+base_model module: Defines the BaseModel class.
+"""
+from uuid import uuid4
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """
+    BaseModel class: Represents a base model with common attributes and methods
+    """
+
+    self.id = str(uuid4())
+    self.created_at = datetime.now()
+    self.updated_at = datetime.now()
+
+
+def save(self):
+    """
+    Updates the 'updated_at' attribute with the current
+    datetime and saves the changes to the storage.
+    """
+    self.updated_at = datetime.now()
+    models.storage.save()
+
+
+def to_dict(self):
+    """
+    Converts the BaseModel object to a dictionary for serialization.
+    """
+
+
+def to_dict(self):
+    """
+    Converts the BaseModel object to a dictionary for serialization.
+    """
+    new_dict = self.__dict__.copy()
+    new_dict["_class_"] = self.__class__.__name__
+    new_dict["created_at"] = self.created_at.isoformat()
+    new_dict["updated_at"] = self.updated_at.isoformat()
+    return new_dict
+
+
+def __str__(self):
+    """
+    Returns a string representation of the BaseModel object
+    """
+    return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
